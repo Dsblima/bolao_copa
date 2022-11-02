ecd8874703938eb65a8b159bd27271ac4b71e3e5
Dsblima <dsbl@ecomp.poli.br>
2022-11-02 14:26:34 -0300

    Create sign in and loading screens

---
+++ mobile/src/screen/SignIn.tsx
@@ -0,0 +1,9 @@
+import { VStack, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <VStack flex={1} bgColor="gray.900" alignItems="center" justifyContent="center">        
+        <Text color="white" fontSize={24}>My first screen </Text>        
+    </VStack>
+  )
+}
