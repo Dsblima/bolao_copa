157a05c8c0160e987a631b59d2e6e6ac2acebf58
Dsblima <dsbl@ecomp.poli.br>
2022-11-06 15:49:22 -0300

    Create singin flow

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,27 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+
 import { THEME } from './src/styles/themes';
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screen/SignIn';
+import { Pools } from './src/screen/Pools';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {
-        fontsLoaded ? <SignIn/> : <Loading/>
-      }
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {
+          fontsLoaded ? <Pools/> : <Loading/>
+        }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/screen/SignIn.tsx
+++ mobile/src/screen/SignIn.tsx
@@ -1,9 +1,29 @@
-import { VStack, Text } from "native-base";
+import { VStack, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons';
+
+import { Button } from "../components/Button";
+
+import Logo from '../assets/logo.svg';
+
+import { useAuth } from "../hooks/useAuth";
 
 export function SignIn() {
+  const { signIn, user } = useAuth();
+  
   return (
-    <VStack flex={1} bgColor="gray.900" alignItems="center" justifyContent="center">        
-        <Text color="white" fontSize={24}>My first screen </Text>        
+    <VStack flex={1} bgColor="gray.900" alignItems="center" justifyContent="center" p={7}>        
+        <Logo width={212} height={40}/>
+
+        <Button 
+          type='SECONDARY'
+          title="Entrar com o Google"
+          leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+          mt={12}
+          onPress={signIn}
+        />
+        <Text color="white" textAlign="center" mt={4}>
+          Não utilizamos nenhuma informação além {'\n'}do seu e-mail para criação de sua conta.
+        </Text>
     </VStack>
   )
 }
