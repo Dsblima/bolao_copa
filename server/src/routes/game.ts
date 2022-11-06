0ab9e83059eaceec223e8f28249fa4eed51370d4
Dsblima <dsbl@ecomp.poli.br>
2022-11-06 16:49:48 -0300

    Refactor the app routes

---
+++ server/src/routes/game.ts
@@ -0,0 +1,6 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prsima";
+
+export async function gameRoutes(fastify: FastifyInstance) {
+
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
 import Fastify from "fastify"
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
-import { z } from "zod"
-import ShortUniqueId from "short-unique-id"
 
-const prisma = new PrismaClient({
-  log: ['query'],
-})
+import { poolRoutes } from "./routes/pool"
+import { guessRoutes } from "./routes/guess"
+import { gameRoutes } from "./routes/game"
+import { authRoutes } from "./routes/auth"
+import { userRoutes } from "./routes/user"
 
 async function boostrap() {
   const fastify = Fastify({
@@ -17,42 +16,11 @@ async function boostrap() {
     origin: true,
   })
 
-  fastify.get('/pools/count', async() => {
-    const count = await prisma.pool.count()
-
-    return {count: count}
-  })
-  
-  fastify.get('/users/count', async() => {
-    const count = await prisma.user.count()
-
-    return {count: count}
-  })
-
-  fastify.get('/guesses/count', async() => {
-    const count = await prisma.guess.count()
-
-    return {count: count}
-  })
-
-  fastify.post('/pools', async(request, reply) => {
-    const createPoolBody = z.object({
-      title: z.string(),
-    })
-    
-    const {title} = createPoolBody.parse(request.body)
-    const generate = new ShortUniqueId({length: 6})
-    const code = String(generate()).toUpperCase()
-    
-    await prisma.pool.create({
-      data: {
-        title,
-        code,
-      }
-    })
-
-    reply.status(201).send({ code })    
-  })
+  await fastify.register(authRoutes);
+  await fastify.register(gameRoutes);
+  await fastify.register(guessRoutes);
+  await fastify.register(poolRoutes);
+  await fastify.register(userRoutes);
 
   await fastify.listen({ port:3333, host: '0.0.0.0' })
 }
